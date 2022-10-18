be25302ba10b7ccad6091194dccdc719c92f398f
Aviram Hassan <aviramyhassan@gmail.com>
2022-10-18 13:32:44 +0300

    were getting close

--- mirrord-sip/src/lib.rs
+++ mirrord-sip/src/lib.rs
@@ -8,7 +8,8 @@
 //     })
 // }
 
-use std::borrow::BorrowMut;
+
+use std::ops::Range;
 
 // /// To know if binary is SIP protected for sure, it's in either cases (based on dyld-852.2 code):
 // /// 1. has __RESTRICT segment -- skipped, didn't see it in the wild
@@ -30,19 +31,60 @@ use std::borrow::BorrowMut;
 //         _ => Ok(false),
 //     }
 // }
-use object::{File, read::macho::FatArch, macho::{FatHeader, CPU_TYPE_ARM64, CPU_SUBTYPE_PTRAUTH_ABI, FatArch64}, BigEndian};
-use anyhow::Result;
+// use object::{File, read::macho::FatArch, macho::{FatHeader, CPU_TYPE_ARM64, CPU_SUBTYPE_PTRAUTH_ABI, FatArch64}, BigEndian};
+use anyhow::{anyhow, Result};
+use object::{BigEndian, macho::{FatHeader, FAT_MAGIC, FatArch64, CPU_TYPE_ARM64, CPU_SUBTYPE_ARM64E, CPU_TYPE_ARM64_32, FatArch32, CPU_SUBTYPE_PTRAUTH_ABI, MachHeader64}, LittleEndian};
 
-pub(crate) fn patch_binary(path: &str, output: &str) -> Result<()> {
-    let data = std::fs::read(path)?;
-    let obj = FatHeader::parse_arch64(&*data)?;
-    for mut file in obj {
-        let cpu_sub_type = file.cpusubtype.get(BigEndian);
-        if file.cputype.get(BigEndian) == CPU_TYPE_ARM64 && (cpu_sub_type & CPU_SUBTYPE_PTRAUTH_ABI != 0) {
-            let file_bytes = object::pod::bytes_of(file);
-            let (res, _) = object::pod::from_bytes_mut::<FatArch64>(file_bytes.as_mut()).unwrap();
-            res.cpusubtype.set(BigEndian, cpu_sub_type ^ CPU_SUBTYPE_PTRAUTH_ABI);
-        }
+pub fn patch_binary(path: &str, output: &str) -> Result<()> {
+    let mut data = std::fs::read(path)?;
+    let (header, _): (&FatHeader,_) = object::from_bytes(&data[..std::mem::size_of::<FatHeader>()]).unwrap();
+    let magic = header.magic.get(BigEndian);
+    if magic != FAT_MAGIC {
+        return Err(anyhow!("invalid magic {magic:?}"));
     }
+    let arch_count = header.nfat_arch.get(BigEndian) as usize;
+    let archs_range = Range {
+        start: std::mem::size_of::<FatHeader>(),
+        end: std::mem::size_of::<FatHeader>() + arch_count * std::mem::size_of::<FatArch32>(),
+    };
+    let (archs, _) : (&mut [FatArch32], _) = object::slice_from_bytes_mut(&mut data[archs_range], arch_count).unwrap();
+    let mut arch_offset = 0;
+    let mut arch_size = 0;
+    for arch in archs {
+        let cpu_type = arch.cputype.get(BigEndian);
+        let cpu_subtype = arch.cpusubtype.get(BigEndian);
+        println!("test: {:?}", cpu_type & CPU_TYPE_ARM64);
+        if cpu_type == CPU_TYPE_ARM64 && (cpu_subtype & CPU_SUBTYPE_ARM64E) > 0 {
+            arch.cpusubtype.set(BigEndian, cpu_subtype ^ CPU_SUBTYPE_ARM64E);
+            arch_offset = arch.offset.get(BigEndian) as usize;
+            arch_size = arch.size.get(BigEndian) as usize;
+            println!("patch1");
+        } else {
+            println!("{cpu_type:?} {cpu_subtype:?}");
+        }
+
+    }
+    let range = Range {
+        start: arch_offset,
+        end: arch_offset + arch_size,
+    };
+    let (arch_header, _): (&mut MachHeader64<LittleEndian>, _) = object::from_bytes_mut(&mut data[range]).unwrap();
+    let cpu_type = arch_header.cputype.get(LittleEndian);
+    let cpu_subtype = arch_header.cpusubtype.get(LittleEndian);
+    println!("magic {:?}", arch_header.magic.get(BigEndian));
+    if cpu_type == CPU_TYPE_ARM64 && (cpu_subtype & CPU_SUBTYPE_ARM64E) > 0 {
+        println!("patch2");
+        arch_header.cpusubtype.set(LittleEndian, cpu_subtype ^ CPU_SUBTYPE_ARM64E);
+    } else {
+        println!("{cpu_type:?} {cpu_subtype:?}");
+    }
+    std::fs::write(output, data).unwrap();
     Ok(())
 }
+
+// pub fn patch_binary(path: &str, _output: &str) -> Result<()> {
+//     let data = std::fs::read(path)?;
+//     let arch = object::macho::FatHeader::parse_arch32(&*data).unwrap();
+
+//     Ok(())
+// }
--- mirrord-sip/src/main.rs
+++ mirrord-sip/src/main.rs
@@ -1,8 +1,10 @@
 mod lib;
-use lib::is_sip;
+// use lib::is_sip;
 
 
 fn main() {
-    let path = std::env::args().last().unwrap();
-    println!("{path:?} is sip: {:?}", is_sip(&path));
+    // let path = std::env::args().last().unwrap();
+    // println!("{path:?} is sip: {:?}", is_sip(&path));
+    lib::patch_binary("/usr/bin/env", "/tmp/env").unwrap();
+    println!("patched");
 }
