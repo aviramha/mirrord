9bedbab9b9fdb846cdd8f3d2de113c9f1d32f349
Aviram Hassan <aviram@metalbear.co>
2023-12-15 21:36:37 +0200

    test

--- mirrord/kube/src/api/container/job.rs
+++ mirrord/kube/src/api/container/job.rs
@@ -301,7 +301,6 @@ impl ContainerVariant for JobTargetedVariant<'_> {
                                 "securityContext": {
                                     "runAsGroup": params.gid,
                                     "runAsNonRoot": true,
-                                    "runAsUser": params.gid,
                                     "privileged": agent.privileged,
                                     "capabilities": {
                                         "add": get_capabilities(agent),
